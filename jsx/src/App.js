a421a64a31ef242e30974272cd6bea836b3796be
Tidiane Diallo <diallotidiane014@gmail.com>
2023-01-24 12:22:29 +0000

    Extracting components

---
+++ jsx/src/App.js
@@ -0,0 +1,6 @@
+function App(){
+    return (
+        <h1>Hi there!</h1>
+    )
+}
+export default App;
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -3,38 +3,12 @@
 // 1-) Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import App from './App';
 //2-) Get a reference to the div element
 const el = document.getElementById('root');
 
 //3-) Tell React to take control of that element
 const root = ReactDOM.createRoot(el);
-
-// 4-) create a component
-function App() {
-    // let message = 'Bye there'
-    // if(Math.random() > 0.5){
-    //     message = "Hi there!";
-    // }
-    // return <h1> {new Date().toLocaleTimeString()}</h1>
-    // return <input style={{border:'3px solid red'}} type= "number" min={5} />
-    // return <textarea autoFocus={true} />
-    // return < input spellCheck={false}/>
-    // return (
-    //     <input placeholder='hi there' />
-    // )
-    //****************************  Exercice    ********************************** */
-    return(
-        <div className="wrapper">
-            <textarea 
-                readOnly
-                maxLength={3}
-                spellCheck= "true"
-                style =  {{backgroundColor: "gray"}}
-            />
-        </div>
-    )
-}
 
 // 5-) show the component at the screen
 root.render(<App />);

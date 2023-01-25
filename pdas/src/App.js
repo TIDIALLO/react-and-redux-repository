a328fa5540dc170ea67dbfa88c0abcec93705fbf
Tidiane Diallo <diallotidiane014@gmail.com>
2023-01-25 16:47:05 +0000

    Building with reusable component and using bulma library css

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,14 +1,51 @@
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImge from "./images/cortana.png";
+import SiriImage from "./images/siri.png"
+import 'bulma/css/bulma.css'
 
 function App() {
     return (
         <div>
-            <div>Personal Digital Assistants</div>
-            <ProfileCard title="Alexa" handle="@alexa99"/>
-            <ProfileCard title="Cortana" handle="@cortana32"/>
-            <ProfileCard title="Siri" handle="@siri12"/>
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <p className="title">Personal Digital Assistants</p>
+                </div>
+            </section>
+
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-4">
+                            <ProfileCard 
+                                title="Alexa" 
+                                handle="@alexa99"
+                                image={AlexaImage}
+                                description="Alexa was created by amazon and helps you get started"
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard 
+                                title="Cortana" 
+                                handle="@cortana32" 
+                                image={CortanaImge}
+                                description="Cortana was made by microsoft. Who knows what it does?"
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard
+                                title="Siri" 
+                                handle="@siri12" 
+                                image={SiriImage}
+                                description="Siri was made by Apple and is being phased out"
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 }
 
 export default App;
+//
--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,13 +1,26 @@
-function ProfileCard(title, handle) {
+function ProfileCard({title, handle, image, description}) {
     // const title = props.title   
     // const handle = props.handle
     //equivaut à
     // const {title, handle} = props
     return (
-        <div>
-            <div>Title is {title}</div>
-            <div>Handle is {handle}</div>
+
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img alt="pda logo" src={image} />
+                </figure>
+            </div>
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4"> {title}</p>
+                    <p className="subtitle is-6"> {handle}</p>
+                </div>
+            </div>
+            <div className="content">{description}</div>
         </div>
-    )
+
+        
+    );
 }
 export default ProfileCard;

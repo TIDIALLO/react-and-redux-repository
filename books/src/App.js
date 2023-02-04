4d2b9072ae9f204020cad5e24c07d1d367c4cb43
Tidiane Diallo <diallotidiane014@gmail.com>
2023-02-04 23:56:48 +0000

    end of section6

--- books/src/App.js
+++ books/src/App.js
@@ -32,6 +32,7 @@ function App() {
 
     return (
         <div className="app">
+            <h1>Reading list</h1>
             <BookList books={books} onDelete={deleteBookById} onEdit={editBooById}/>
             <BookCreate onCreate={createBoot}/>
         </div>

718a0fb2a82e8b3abd2f6babdee4911633290e7e
Tidiane Diallo <diallotidiane014@gmail.com>
2023-02-04 16:32:34 +0000

    section6: How to handle forms

---
+++ books/src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+
+function App() {
+    const [books, setBooks] = useState([]);
+    
+    const createBoot =(title) =>{
+        const updatedBooks = [
+            ...books,
+            {id:Math.round(Math.random() * 9999), title:title}
+        ];   
+        setBooks(updatedBooks);
+    }
+
+    const deleteBookById = (id) => {
+        const updateBooks = books.filter((book) => {
+            return book.id !== id;
+        });
+        setBooks(updateBooks);
+    }
+
+    const editBooById = (id, newTitlle) => {
+        const updatedBook = books.map((book) =>{
+            if(book.id === id){
+                return {...book, title:newTitlle}
+            }
+            return book;
+        });
+        setBooks(updatedBook);
+    };
+
+    return (
+        <div className="app">
+            <BookList books={books} onDelete={deleteBookById} onEdit={editBooById}/>
+            <BookCreate onCreate={createBoot}/>
+        </div>
+    );
+}
+
+export default App;

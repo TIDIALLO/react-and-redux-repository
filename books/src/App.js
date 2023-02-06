2d5ac6e634f15ea655ab40899826378b799c867b
Tidiane Diallo <diallotidiane014@gmail.com>
2023-02-06 16:22:36 +0000

    end of section 7: data persistence with API request

--- books/src/App.js
+++ books/src/App.js
@@ -1,29 +1,48 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from "axios";
 
 function App() {
     const [books, setBooks] = useState([]);
     
-    const createBoot =(title) =>{
-        const updatedBooks = [
-            ...books,
-            {id:Math.round(Math.random() * 9999), title:title}
-        ];   
+    const fetchBooks = async () =>{
+        const response = await axios.get('http://localhost:3001/books');
+
+        setBooks(response.data);
+    }
+
+    useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    const createBoot = async (title) =>{
+        const response = await axios.post("http://localhost:3001/books", 
+            title,
+        )
+        console.log(response);
+        const updatedBooks = [...books, response.data];   
         setBooks(updatedBooks);
     }
 
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
+        
         const updateBooks = books.filter((book) => {
             return book.id !== id;
         });
         setBooks(updateBooks);
     }
 
-    const editBooById = (id, newTitlle) => {
+    const editBooById = async (id, newTitlle) => {
+        const response = await axios.put(`http://localhost:3001/${id}`,{
+            title:newTitlle,
+        });
+        console.log(response);
+
         const updatedBook = books.map((book) =>{
             if(book.id === id){
-                return {...book, title:newTitlle}
+                return {...book, ...response.data}
             }
             return book;
         });
@@ -33,7 +52,7 @@ function App() {
     return (
         <div className="app">
             <h1>Reading list</h1>
-            <BookList books={books} onDelete={deleteBookById} onEdit={editBooById}/>
+            <BookList  onDelete={deleteBookById} books={books} onEdit={editBooById}/>
             <BookCreate onCreate={createBoot}/>
         </div>
     );

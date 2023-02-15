65b0a985c2fe3a7f2c0b01e902d27c37ab3afc46
Tidiane Diallo <diallotidiane014@gmail.com>
2023-02-15 12:34:04 +0000

    Make a feature-full data table

--- comps/src/App.js
+++ comps/src/App.js
@@ -4,6 +4,7 @@ import AccordionPage from "./pages/AccordionPage";
 import DropdownPage from "./pages/DropdownPage";
 import ButtonPage from "./pages/ButtonPage";
 import ModalPage from "./pages/ModelPage";
+import TablePage from "./pages/TablePage";
 function App() {
      return (
             <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
@@ -21,9 +22,12 @@ function App() {
                 <Route path="/modals">
                     <ModalPage  />
                 </Route>
+                <Route path="/table">
+                    <TablePage  />
+                </Route>
             </div>
             </div>
-          );
+        );
 }
 
 export default App;

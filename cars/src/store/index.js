dbb6fcb956fd6fb8ec43b9f63007e6c4004cd830
Tidiane Diallo <diallotidiane014@gmail.com>
2023-02-27 11:18:48 +0000

    Managing multiple slices with redux toolkit: cars

---
+++ cars/src/store/index.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { carsReducer, addCar, removeCar, changeSearchTerm } from "./slices/carsSlice";
+import { formReducer, changeName, changeCost } from "./slices/formSlice";
+
+const store = configureStore({
+    reducer: {
+        car: carsReducer,
+        form: formReducer
+    }
+});
+export { store, changeName, changeCost, addCar, removeCar, changeSearchTerm }

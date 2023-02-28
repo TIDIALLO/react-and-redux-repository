99103e6d09a47f07c3081fa691c2343df42c87ed
Tidiane Diallo <diallotidiane014@gmail.com>
2023-02-28 13:42:48 +0000

    interfacing with API's using Async Thuncks: media [Animation with Tailwind css]

---
+++ media/src/store/thunks/fetchUsers.js
@@ -0,0 +1,17 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchUsers = createAsyncThunk('users/fetch', async () => {
+    const response = await axios.get('http://localhost:3005/users');
+
+    //EDEV ONLY
+    await pause(3000);
+    return response.data;
+});
+
+// DEV ONLY !!!
+const pause =(duration) =>{
+    return new Promise((resolve) =>{
+        setTimeout(resolve, duration);
+    });
+};

5448825b02922e0ca51aab7552d468d0e89e6a90
Tidiane Diallo <diallotidiane014@gmail.com>
2023-03-05 03:12:32 +0000

    fin du projet

--- media/src/store/apis/photosApi.js
+++ media/src/store/apis/photosApi.js
@@ -1,51 +1,51 @@
-import { faker } from "@faker-js/faker";
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+import { faker } from '@faker-js/faker';
 
 const photosApi = createApi({
     reducerPath: 'photos',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost/3005',
+        baseUrl: 'http://localhost:3005',
     }),
     endpoints(builder) {
         return {
             fetchPhotos: builder.query({
-                query:(album)=>{
-                    return{
+                query: (album) => {
+                    return {
                         url: '/photos',
                         params: {
-                            albumId: album.id
-                        },
-                        method: 'GET'
-                    }
-                }
-            }),
-            addPhoto: builder.mutation({
-                query:(album)=>{
-                    return{ 
-                        mathod: 'POST',
-                        url: '/photos',
-                        body:{
                             albumId: album.id,
-                            url: faker.image.abstract(150,150,true)
-                        }
-                    }
-                }
-            }),
-            removePhoto: builder.mutation({
-                query:(photo)=>{
-                    return{
-                        method: 'DELETE',
-                        url: `/photos/${photo.id}`
-                    }
+                        },
+                        method: 'GET',
+                    };
                 },
             }),
-        }
-    }
+            addPhoto: builder.mutation({
+                query: (album) => {
+                    return {
+                        method: 'POST',
+                        url: '/photos',
+                        body: {
+                            albumId: album.id,
+                            url: faker.image.abstract(150, 150, true),
+                        },
+                    };
+                },
+            }),
+            removePhoto: builder.mutation({
+                query: (photo) => {
+                    return {
+                        method: 'DELETE',
+                        url: `/photos/${photo.id}`,
+                    };
+                },
+            }),
+        };
+    },
 });
 
 export const {
     useFetchPhotosQuery,
-    useAddPhotoMutatoin,
-    removePhotoMutatoin,
+    useAddPhotoMutation,
+    useRemovePhotoMutation,
 } = photosApi;
-export {photosApi};
+export { photosApi };
--- media/src/store/index.js
+++ media/src/store/index.js
@@ -1,14 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit"
-import { setupListeners } from "@reduxjs/toolkit/dist/query";
-import { usersReducer } from "./slices/usersSlice"
-import { albumsApi } from "./apis/albumsApi";
-import { photosApi } from "./apis/photosApi";
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { usersReducer } from './slices/usersSlice';
+import { albumsApi } from './apis/albumsApi'; 
+import { photosApi } from './apis/photosApi';
 
 export const store = configureStore({
     reducer: {
         users: usersReducer,
         [albumsApi.reducerPath]: albumsApi.reducer,
-        [photosApi.reducerPath]: photosApi.reducer
+        [photosApi.reducerPath]: photosApi.reducer,
     },
     middleware: (getDefaultMiddleware) => {
         return getDefaultMiddleware()
@@ -19,8 +19,8 @@ export const store = configureStore({
 
 setupListeners(store.dispatch);
 
-export * from "./thunks/fetchUsers";
-export * from "./thunks/addUser";
+export * from './thunks/fetchUsers';
+export * from './thunks/addUser';
 export * from './thunks/removeUser';
 export {
     useFetchAlbumsQuery,
@@ -29,6 +29,6 @@ export {
 } from './apis/albumsApi';
 export {
     useFetchPhotosQuery,
-    useAddPhotoMutatoin,
-    removePhotoMutatoin,
+    useAddPhotoMutation,
+    useRemovePhotoMutation,
 } from './apis/photosApi';
